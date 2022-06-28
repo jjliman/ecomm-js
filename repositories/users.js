605a14e77af1fec9ba47e6ecd21ec6df89280f64
Justin Liman <justinliman97@gmail.com>
2022-06-28 13:24:12 -0700

    implement user repo constructor

--- repositories/users.js
+++ repositories/users.js
@@ -1,3 +1,25 @@
+const fs = require('fs');
+
 class UsersRepository {
-    
+    constructor(filename) {
+        if (!filename) {
+            throw new Error('Creating a repository requires a file name!');
+        }
+        this.filename = filename;
+        try {
+            fs.accessSync(this.filename);
+        } catch (err) {
+            fs.writeFileSync(this.filename, '[]');
+        }
+    }
+
+    async getAll() {
+        // Open the file this.filename
+        const contents = await fs.promises.readFile(this.filename, { encoding: 'utf-8' });
+        console.log(contents);
+    }
+}
+
+const test = () => {
+    const repo = new UsersRepository('users.json');;
 }
