887f368b6c6f0ec9600d25bb105f679e2b35c7e8
Justin Liman <justinliman97@gmail.com>
2022-06-28 01:20:07 -0700

    add and initialize express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hi there!');
+});
+
+app.listen(3000, () => {
+    console.log('Listening');
+})
+
