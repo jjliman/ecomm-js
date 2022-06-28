5c5d4eaaf450674f8b03c72cc32003f665743acf
Justin Liman <justinliman97@gmail.com>
2022-06-28 04:07:46 -0700

    put urlencoded middleware into app use

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser'); // Not needed!
 
 const app = express();
+
+
+// Newer version of express has urleconded, so can just do express(NOT app!).urlencoded(), no need bodyParser
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send(`
@@ -15,26 +20,26 @@ app.get('/', (req, res) => {
     `);
 });
 
-const bodyParser = (req, res, next) => {
-    if (req.method === 'POST') {
-        req.on('data', (data) => {
-            const parsed = data.toString('utf8').split('&');
-            // console.log(parsed);
-            const formData = {};
-            for (let pair of parsed) {
-                const [key, value] = pair.split('=');
-                // console.log(`${key} is ${value}`)
-                formData[key] = value;
-            }
-            req.body = formData;
-            next();
-        });
-    } else {
-        next();
-    }
-};
+// const bodyParser = (req, res, next) => {
+//     if (req.method === 'POST') {
+//         req.on('data', (data) => {
+//             const parsed = data.toString('utf8').split('&');
+//             // console.log(parsed);
+//             const formData = {};
+//             for (let pair of parsed) {
+//                 const [key, value] = pair.split('=');
+//                 // console.log(`${key} is ${value}`)
+//                 formData[key] = value;
+//             }
+//             req.body = formData;
+//             next();
+//         });
+//     } else {
+//         next();
+//     }
+// };
 
-app.post('/', bodyParser, (req, res) => {
+app.post('/', (req, res) => {
     console.log(req.on, req.body, req.method);
     console.log('POST REQUEST HANDLER');
     res.send('Account created!!');
