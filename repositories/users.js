34a3318486945054e8d7a4bb802392cf9d8ec94b
Justin Liman <justinliman97@gmail.com>
2022-06-28 14:59:23 -0700

    refactor writeAll method

--- repositories/users.js
+++ repositories/users.js
@@ -24,6 +24,10 @@ class UsersRepository {
     async create(attrs) {
         const records = await this.getAll();
         records.push(attrs);
+        await this.writeAll(records);
+    }
+
+    async writeAll(records) {
         await fs.promises.writeFile(this.filename, JSON.stringify(records));
     }
 }
