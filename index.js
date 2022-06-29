41382624dca473d5d2c9717e5a78ac12123ccd69
Justin Liman <justinliman97@gmail.com>
2022-06-29 00:57:28 -0700

    add cookie session

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser'); // Not needed!
+const cookieSession = require('cookie-session');
 const usersRepo = require('./repositories/users');
 
 const app = express();
@@ -7,6 +8,9 @@ const app = express();
 
 // Newer version of express has urleconded, so can just do express(NOT app!).urlencoded(), no need bodyParser
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieSession( {
+    keys: ['aascohjzc3934901odc']
+}));
 
 app.get('/', (req, res) => {
     res.send(`
