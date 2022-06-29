b9fa3ee0c3a8ade72d32184376a25c280e62bc45
Justin Liman <justinliman97@gmail.com>
2022-06-29 01:14:59 -0700

    implement sign out

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cookieSession( {
     keys: ['aascohjzc3934901odc']
 }));
 
-app.get('/', (req, res) => {
+app.get('/signup', (req, res) => {
     res.send(`
         <div>
             Your id is: ${req.session.userId}
@@ -45,7 +45,7 @@ app.get('/', (req, res) => {
 //     }
 // };
 
-app.post('/', async (req, res) => {
+app.post('/signup', async (req, res) => {
     // console.log(req.body);
     const { email, password, passwordConfirmation } = req.body;
 
@@ -66,7 +66,28 @@ app.post('/', async (req, res) => {
     res.send('Account created!!');
 });
 
+app.get('/signout', (req, res) => {
+    req.session = null;
+    res.send('You are logged out!');
+});
+
+app.get('/signin', (req, res) => {
+    res.send(`
+        <div>
+            <form method="POST">
+                <input name="email" placeholder="email">
+                <input name="password" placeholder="password">
+                <button>Sign In</button>
+            </form>
+        </div>
+    `);
+});
+
+app.post('/signin', (req, res) => {
+    const { email, password } = req.body;
+});
+
 app.listen(3000, () => {
     console.log('Listening');
-})
+});
 
