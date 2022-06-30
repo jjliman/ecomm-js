c0af4bfc863ce7b1d3155d2278b4f239f474f2c9
Justin Liman <justinliman97@gmail.com>
2022-06-29 22:04:40 -0700

    add validators and custom validators

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -35,31 +35,38 @@ router.get('/signup', (req, res) => {
 router.post(
     '/signup',
      [
-        check('email').trim().normalizeEmail().isEmail(),
+        check('email')
+            .trim()
+            .normalizeEmail()
+            .isEmail()
+            .custom(async (email) => {
+                const existingUser = await usersRepo.getOneBy({ email });
+                if (existingUser) {
+                    throw new Error('Email in use!');
+                }
+            }),
         check('password').trim().isLength({ min: 4, max: 20 }),
-        check('passwordConfirmation').trim().isLength({ min: 4, max: 20 })
+        check('passwordConfirmation')
+            .trim()
+            .isLength({ min: 4, max: 20 })
+            .custom((passwordConfirmation, { req }) => {
+                if (passwordConfirmation !== req.body.password) {
+                    throw new Error('Passwords must match!');
+                }
+            })
     ], 
     async (req, res) => {
         const errors = validationResult(req);
         console.log(errors);
-    // console.log(req.body);
-    const { email, password, passwordConfirmation } = req.body;
+        // console.log(req.body);
+        const { email, password, passwordConfirmation } = req.body;
 
-    const existingUser = await usersRepo.getOneBy({ email });
-    if (existingUser) {
-        return res.send('Email in use!');
-    }
+        const user = await usersRepo.create({ email, password });
 
-    if (password !== passwordConfirmation) {
-        return res.send('Passwords must match!');
-    }
+        req.session.userId = user.id;
 
-    const user = await usersRepo.create({ email, password });
-
-    req.session.userId = user.id;
-
-    // console.log('POST REQUEST HANDLER');
-    res.send('Account created!!');
+        // console.log('POST REQUEST HANDLER');
+        res.send('Account created!!');
 });
 
 router.get('/signout', (req, res) => {
