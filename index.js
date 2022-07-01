31627220f893f0035c6391d1a224ab1ff151fab3
Justin Liman <justinliman97@gmail.com>
2022-07-01 03:29:10 -0700

    add shopping cart boilerplate

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
 const adminProductsRouter = require('./routes/admin/products');
 const productsRouter = require('./routes/products');
-const products = require('./repositories/products');
+const cartsRouter = require('./routes/carts');
 
 const app = express();
 
@@ -19,6 +19,7 @@ app.use(cookieSession( {
 app.use(authRouter);
 app.use(adminProductsRouter);
 app.use(productsRouter);
+app.use(cartsRouter);
 
 
 app.listen(3000, () => {
---
+++ routes/carts.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const cartsRepo = require('../repositories/carts');
+
+const router = express.Router();
+
+// router.get('/carts');
+
+module.exports = router;
